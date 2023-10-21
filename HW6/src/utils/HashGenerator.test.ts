40031124e5c7232ec05679a38ed87ec93940821d
Jerry <71301727+Jerry0424@users.noreply.github.com>
2023-10-22 00:55:41 +0800

    ThirdCase finish

--- HW6/src/utils/HashGenerator.test.ts
+++ HW6/src/utils/HashGenerator.test.ts
@@ -1,5 +1,34 @@
-describe("Need implement", () => {
-    test("Todo", () => {
-        expect(1).toEqual(1);
-    });
+
+import { HashGenerator } from './HashGenerator'
+
+describe('HashGenerator', () => {
+  let hashGenerator: HashGenerator;
+
+  beforeEach(() => {
+    hashGenerator = new HashGenerator();
+  });
+
+  test('should generate a random string consist of letters A~Z', () => {
+    const mockMathRandom = jest.spyOn(Math, 'random');
+    mockMathRandom.mockReturnValue(0.5); 
+
+    expect(hashGenerator.g(8)).toMatch(/^[A-Z]{8}$/);
+
+    mockMathRandom.mockRestore();
+  });
+
+  test('should throw an error if the input length is not greater than 0', () => {
+    expect(() => {hashGenerator.g(0);}).toThrow("Hash number can't less than 0");
+  });
+
+  test('should generate a simple ISBN string with numbers and dashes', () => {
+    const mockMathRandom = jest.spyOn(Math, 'random');
+    mockMathRandom.mockReturnValue(0.5); 
+
+    const zeroString = '000-00-00000-00-0';
+    expect( hashGenerator.simpleISBN(zeroString)).toMatch(/^\d{3}-\d{2}-\d{5}-\d{2}-\d{1}$/);
+
+    mockMathRandom.mockRestore(); 
+  });
+
 });
